b5889f7deab0d31e63c26ac2b0a9f69396673036
Michael Gries <github@gries.name>
2016-10-06 00:50:13 +0200

    Add files via upload

--- hardware/Sonoff/Blink.ino
+++ hardware/Sonoff/Blink.ino
@@ -9,26 +9,47 @@
   
   by Michael Gries 
   
+   LINKS:
+   see http://esp8266.github.io/Arduino/versions/2.1.0-rc2/doc/libraries.html#esp-specific-apis
+
+  
  */
  
-#define LEDgreen 13  // ITEAD Sonoff LED (GPIO13)
+#define LEDgreen 13  // ITEAD Sonoff LED (GPIO13) - Pin 12
+#define Btn_E_FW 0   // ITEAD Sonoff Button (GPIO0) - Pin 15
 #define LEDblue 2  // NodeMCU blue LED (pin D0)
 #define LEDred 16  // NodeMCU red  LED (pin D4)
+
+int valBtn = HIGH; 
+
+void printESPdiagnosis(){
+    Serial.println(ESP.getFreeHeap());
+    Serial.println(ESP.getChipId());
+    Serial.println(ESP.getFlashChipId());
+    Serial.println(ESP.getFlashChipSize());
+    Serial.println(ESP.getFlashChipSpeed());
+    Serial.println(ESP.getCycleCount());
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(LEDgreen, OUTPUT);
+  pinMode(Btn_E_FW, INPUT);
   Serial.begin(74880);
   Serial.print("\n\rBlink example for ITEAD Sonoff devive reprogramming test ...");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial.print(".");             // keep alive message on Serial Monitor
   digitalWrite(LEDgreen, LOW);    // turn the LED off by making the voltage LOW
-  Serial.print("\n\rLED on  ...");
+  Serial.println("LED on  ...");
   delay(1000);                   // wait for a second
   digitalWrite(LEDgreen, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.print("\n\rLED off ...");
+  Serial.println("LED off ...");
   delay(1000);                   // wait for a second
+  valBtn = digitalRead(Btn_E_FW);
+  if(valBtn == LOW) {
+    Serial.println("Button pressed:");
+    printESPdiagnosis();
+  }
 }
